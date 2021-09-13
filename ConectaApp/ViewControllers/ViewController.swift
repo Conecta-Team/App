fcbe08cf9f70930f0dc5f63b177426ed487403f1
Joao Silva <josejoaosilva.n@gmail.com>
2021-09-13 08:27:09 -0300

    create project, configure lint and created folders

---
+++ ConectaApp/ViewControllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ConectaApp
+//
+//  Created by José João Silva Nunes Alves on 13/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view = View()
+    }
+}
---
+++ ConectaApp/ViewControllers/Views/View.swift
@@ -0,0 +1,16 @@
+//
+//  View.swift
+//  ConectaApp
+//
+//  Created by José João Silva Nunes Alves on 13/09/21.
+//
+
+import UIKit
+
+class View: UIView {
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.backgroundColor = .gray
+    }
+}
