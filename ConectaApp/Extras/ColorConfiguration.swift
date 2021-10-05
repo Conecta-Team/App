d6ac7fe1ea9ef72ea091cc56cb34144453957470
Helaine <helaine.alves.pontes07@aluno.ifce.edu.br>
2021-10-05 18:53:25 -0300

    Alteração das cores

--- ConectaApp/Extras/ColorConfiguration.swift
+++ ConectaApp/Extras/ColorConfiguration.swift
@@ -10,23 +10,21 @@ import UIKit
 
 extension UIColor {
 
-    static let darkPurple = UIColor(red: 0.33, green: 0.00, blue: 0.77, alpha: 1.00)
-    static let lightPurple = UIColor(red: 0.93, green: 0.90, blue: 0.98, alpha: 1.00)
-    static let textPurple = UIColor(red: 0.09, green: 0.00, blue: 0.19, alpha: 1.00)
-
-    static let darkOrange = UIColor(red: 0.91, green: 0.32, blue: 0.14, alpha: 1.00)
-    static let lightOrange = UIColor(red: 0.99, green: 0.93, blue: 0.91, alpha: 1.00)
-    static let textBrown = UIColor(red: 0.52, green: 0.18, blue: 0.08, alpha: 1.00)
-
-    static let darkBlue = UIColor(red: 0.00, green: 0.66, blue: 0.73, alpha: 1.00)
-    static let lightBlue = UIColor(red: 0.90, green: 0.96, blue: 0.97, alpha: 1.00)
-    static let textGreen = UIColor(red: 0.00, green: 0.16, blue: 0.18, alpha: 1.00)
-    static let textGreen2 = UIColor(red: 0.00, green: 0.37, blue: 0.41, alpha: 1.00)
-
-    static let actionPink = UIColor(red: 0.97, green: 0.00, blue: 0.36, alpha: 1.00)
-
-    static let backgroundGray = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1.00)
-    static let textLightGray = UIColor(red: 0.62, green: 0.62, blue: 0.62, alpha: 1.00)
-    static let textDarkGray = UIColor(red: 0.25, green: 0.25, blue: 0.25, alpha: 1.00)
-
+    static let purple100 = UIColor(red: 0.19, green: 0.01, blue: 0.44, alpha: 1.00)
+    static let purple80 = UIColor(red: 0.33, green: 0.00, blue: 0.77, alpha: 1.00)
+    static let purple50 = UIColor(red: 0.73, green: 0.64, blue: 0.97, alpha: 1.00)
+    static let purple20 = UIColor(red: 0.93, green: 0.90, blue: 0.98, alpha: 1.00)
+    
+    static let green100 = UIColor(red: 0.00, green: 0.37, blue: 0.41, alpha: 1.00)
+    static let green80 = UIColor(red: 0.00, green: 0.66, blue: 0.73, alpha: 1.00)
+    static let green50 = UIColor(red: 0.76, green: 0.92, blue: 0.94, alpha: 1.00)
+    static let green20 = UIColor(red: 0.90, green: 0.96, blue: 0.97, alpha: 1.00)
+    
+    static let pink100 = UIColor(red: 0.80, green: 0.00, blue: 0.30, alpha: 1.00)
+    static let pink80 = UIColor(red: 0.97, green: 0.00, blue: 0.36, alpha: 1.00)
+    static let pink50 = UIColor(red: 0.95, green: 0.17, blue: 0.46, alpha: 1.00)
+    static let pink20 = UIColor(red: 0.98, green: 0.69, blue: 0.80, alpha: 1.00)
+    
+    static let alertRed = UIColor(red: 1.00, green: 0.00, blue: 0.00, alpha: 1.00)
+    static let textGray = UIColor(red: 0.54, green: 0.54, blue: 0.54, alpha: 1.00)
 }
