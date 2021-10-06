7c6c44182cb5b276e777f31001260e9e8e94ea16
Helaine <helaine.alves.pontes07@aluno.ifce.edu.br>
2021-10-06 09:16:03 -0300

    Voltando com ascores antigas, para depois apaga-las

--- ConectaApp/Extras/ColorConfiguration.swift
+++ ConectaApp/Extras/ColorConfiguration.swift
@@ -27,4 +27,24 @@ extension UIColor {
     
     static let alertRed = UIColor(red: 1.00, green: 0.00, blue: 0.00, alpha: 1.00)
     static let textGray = UIColor(red: 0.54, green: 0.54, blue: 0.54, alpha: 1.00)
+    
+    //antigo
+    static let darkPurple = UIColor(red: 0.33, green: 0.00, blue: 0.77, alpha: 1.00)
+    static let lightPurple = UIColor(red: 0.93, green: 0.90, blue: 0.98, alpha: 1.00)
+    static let textPurple = UIColor(red: 0.09, green: 0.00, blue: 0.19, alpha: 1.00)
+
+    static let darkOrange = UIColor(red: 0.91, green: 0.32, blue: 0.14, alpha: 1.00)
+    static let lightOrange = UIColor(red: 0.99, green: 0.93, blue: 0.91, alpha: 1.00)
+    static let textBrown = UIColor(red: 0.52, green: 0.18, blue: 0.08, alpha: 1.00)
+
+    static let darkBlue = UIColor(red: 0.00, green: 0.66, blue: 0.73, alpha: 1.00)
+    static let lightBlue = UIColor(red: 0.90, green: 0.96, blue: 0.97, alpha: 1.00)
+    static let textGreen = UIColor(red: 0.00, green: 0.16, blue: 0.18, alpha: 1.00)
+    static let textGreen2 = UIColor(red: 0.00, green: 0.37, blue: 0.41, alpha: 1.00)
+
+    static let actionPink = UIColor(red: 0.97, green: 0.00, blue: 0.36, alpha: 1.00)
+
+    static let backgroundGray = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1.00)
+    static let textLightGray = UIColor(red: 0.62, green: 0.62, blue: 0.62, alpha: 1.00)
+    static let textDarkGray = UIColor(red: 0.25, green: 0.25, blue: 0.25, alpha: 1.00)
 }
