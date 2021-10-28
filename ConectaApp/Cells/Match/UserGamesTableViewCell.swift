965e70b3ad428b2cfa5f0a88d4dd17ff7278ac6a
Joao Silva <josejoaosilva.n@gmail.com>
2021-10-28 17:48:14 -0300

    delete duplicatedes archives

--- ConectaApp/Cells/Match/UserGamesTableViewCell.swift
+++ ConectaApp/Cells/Match/UserGamesTableViewCell.swift
@@ -57,7 +57,6 @@ class UserGamesTableViewCell: UITableViewCell {
     public func configure(games: [Games]) {
         self.games = games
 
-        // TODO: ainda precisa ser uma collection aqui
         if let game = self.games.first {
             self.gameName.text = game.name
         }
