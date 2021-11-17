740e778f415e370eef766dbcff5b60e37abeeb60
Helaine <helaine.alves.pontes07@aluno.ifce.edu.br>
2021-11-17 09:08:11 -0300

    Alterando cotainer do cloudKit e versão do app

--- ConectaApp/Services/CloudKitService/CloudKitService.swift
+++ ConectaApp/Services/CloudKitService/CloudKitService.swift
@@ -13,7 +13,7 @@ class CloudKitService {
     var privateDatabase: CKDatabase
 
     init() {
-        container = CKContainer(identifier: "iCloud.ConectApp")
+        container = CKContainer(identifier: "iCloud.ConectaDataBase")
         publicDatabase = container.publicCloudDatabase
         privateDatabase = container.privateCloudDatabase
     }
